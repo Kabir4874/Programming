929e126c09484b9102b1ea4a6d81893d4eba4cbb
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-07-26 19:57:50 +0600

    add

---
+++ Algorithms/Tracker Sheet/Adhoc/5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, n, x, y, d;
+    cin >> t;
+    while (t--)
+    {
+
+        cin >> n >> x >> y >> d;
+        
+    }
+    return 0;
+}
