9075c16f52afb22b9c15e8fe4eac04c01c538335
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-09 22:10:46 +0600

    add

--- Data Structure &  Algorithms/Course Class Practice/Lecture 25/play.cpp
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 25/play.cpp
@@ -6,10 +6,30 @@ int main()
 
     // cout << *p << endl;
 
-    int i = 5;
-    int *p = &i;
-    cout << p << endl;
-    cout << *p << endl;
+    // int i = 5;
+    // int *p = &i;
+    // cout << p << endl;
+    // cout << *p << endl;
+
+    // int num = 5;
+    // int a = num;
+    // a++;
+
+    // cout << num << endl;
+    // cout << a << endl;
+
+    // int num = 5;
+    // int *p = &num;
+    // (*p)++;
+    // cout << num << endl;
+
+    // int *q = p;
+    // cout << *q << endl;
+
+    int i = 3;
+    int *t = &i;
+    cout << (*t)++ << endl;
+    cout << ++t << endl;
 
     return 0;
 }
