a27bbaf6fcbcc4ba9e98949296e2f29b0fe5def6
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-10 23:54:19 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 26/pointer_Array.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    int arr[10] = {23, 122, 41, 67};
+    cout << "Address of first memory block is: " << arr << endl;
+    cout << "Address of first memory block is: " << &arr[0] << endl;
+    cout << "4th " << *arr << endl;
+    cout << "5th " << *arr + 1 << endl;
+    cout << "6th " << *(arr + 1) << endl;
+    cout << "7th " << *(arr) + 1 << endl;
+    cout << "8th " << arr[2] << endl;
+    cout << "9th " << *(arr + 2) << endl;
+    return 0;
+}
