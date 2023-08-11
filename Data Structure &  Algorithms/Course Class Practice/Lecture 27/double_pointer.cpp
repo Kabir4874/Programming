77db7d75bb2c4ceefed94bb4eff9eb4f57581067
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-11 20:11:21 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 27/double_pointer.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void update(int **p2)
+{
+    // p2 = p2 + 1;
+    *p2 = *p2 + 1;
+    // **p2 = **p2 + 1;
+}
+int main()
+{
+    int i = 5;
+    int *p = &i;
+    int **p2 = &p;
+
+    // cout << p << endl;
+    // cout << **p2 << endl;
+
+    // cout << &i << endl;
+
+    cout << endl;
+    cout << "Before: " << i << endl;
+    cout << "Before: " << p << endl;
+    cout << "Before: " << p2 << endl;
+    update(p2);
+    cout << "After: " << i << endl;
+    cout << "After: " << p << endl;
+    cout << "After: " << p2 << endl;
+    return 0;
+}
