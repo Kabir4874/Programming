467fb489efd018a415c5a42a5c1ed6aa428c90ec
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-12 19:53:31 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 28/ReferenceVariable.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void update2(int &n)
+{
+    n++;
+}
+void update(int n)
+{
+    n++;
+}
+int &func(int a)
+{
+    int num = a;
+    int &ans = num;
+    return ans;
+}
+int main()
+{
+    // int i = 5;
+    // int &j = i;
+    // cout << i << endl;
+    // i++;
+    // cout << i << endl;
+    // j++;
+    // cout << i << endl;
+    // cout << j << endl;
+
+    int n = 5;
+    cout << "Before: " << n << endl;
+    // update(n);
+    update2(n);
+    cout << "After: " << n << endl;
+
+    func(n);
+
+    return 0;
+}
