e9481c857610405275a5ced3c5339fde0385af7f
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-13 23:40:18 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 32/stairs.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int stairs(int n)
+{
+    if (n < 0)
+    {
+        return 0;
+    }
+    if (n == 0)
+    {
+        return 1;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = stairs(n);
+    return 0;
+}
