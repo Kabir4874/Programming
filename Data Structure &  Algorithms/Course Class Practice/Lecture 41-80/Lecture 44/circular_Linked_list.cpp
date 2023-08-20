a75fe4f0c084bfa4f3a3e0fb84ce01dc7f26dcaf
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-20 22:40:40 +0600

    add

--- Data Structure &  Algorithms/Course Class Practice/Lecture 41-80/Lecture 44/circular_Linked_list.cpp
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 41-80/Lecture 44/circular_Linked_list.cpp
@@ -1,192 +1,209 @@
-#include<iostream>
-#include<map>
+#include <iostream>
+#include <map>
 using namespace std;
 
-class Node {
-    public:
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    //constrcutor
-    Node(int d) {
+    // constrcutor
+    Node(int d)
+    {
         this->data = d;
         this->next = NULL;
     }
 
-    ~Node() {
+    ~Node()
+    {
         int value = this->data;
-        if(this->next != NULL) {
+        if (this->next != NULL)
+        {
             delete next;
             next = NULL;
         }
         cout << " memory is free for node with data " << value << endl;
     }
-
 };
 
-void insertNode(Node* &tail, int element, int d) {
-    
+void insertNode(Node *&tail, int element, int d)
+{
 
-    //empty list
-    if(tail == NULL) {
-        Node* newNode = new Node(d);
+    // empty list
+    if (tail == NULL)
+    {
+        Node *newNode = new Node(d);
         tail = newNode;
-        newNode -> next = newNode;
+        newNode->next = newNode;
     }
-    else{
-        //non-empty list
-        //assuming that the element is present in the list
+    else
+    {
+        // non-empty list
+        // assuming that the element is present in the list
 
-        Node* curr = tail;
+        Node *curr = tail;
 
-        while(curr->data != element) {
-            curr = curr -> next;
+        while (curr->data != element)
+        {
+            curr = curr->next;
         }
-        
-        //element found -> curr is representing element wala node
-        Node* temp = new Node(d);
-        temp -> next = curr -> next;
-        curr -> next = temp;
 
+        // element found -> curr is representing element wala node
+        Node *temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+}
+
+void print(Node *tail)
+{
+
+    Node *temp = tail;
+
+    // empty list
+    if (tail == NULL)
+    {
+        cout << "List is Empty " << endl;
+        return;
     }
 
-}    
-
-void print(Node* tail) {
-
-    Node* temp = tail;
-
-    //empty list
-    if(tail == NULL) {
-        cout << "List is Empty "<< endl;
-        return ;
-    }
-
-    do {
-        cout << tail -> data << " ";
-        tail = tail -> next;
-    } while(tail != temp);
+    do
+    {
+        cout << tail->data << " ";
+        tail = tail->next;
+    } while (tail != temp);
 
     cout << endl;
-} 
+}
 
-void deleteNode(Node* &tail, int value) {
+void deleteNode(Node *&tail, int value)
+{
 
-    //empty list
-    if(tail == NULL) {
+    // empty list
+    if (tail == NULL)
+    {
         cout << " List is empty, please check again" << endl;
         return;
     }
-    else{
-        //non-empty
+    else
+    {
+        // non-empty
 
-        //assuming that "value" is present in the Linked List
-        Node* prev = tail;
-        Node* curr = prev -> next;
+        // assuming that "value" is present in the Linked List
+        Node *prev = tail;
+        Node *curr = prev->next;
 
-        while(curr -> data != value) {
+        while (curr->data != value)
+        {
             prev = curr;
-            curr = curr -> next;
+            curr = curr->next;
         }
 
-        prev -> next = curr -> next;
+        prev->next = curr->next;
 
-        //1 Node Linked List
-        if(curr == prev) {
+        // 1 Node Linked List
+        if (curr == prev)
+        {
             tail = NULL;
         }
 
         //>=2 Node linked list
-        else if(tail == curr ) {
+        else if (tail == curr)
+        {
             tail = prev;
         }
 
-        curr -> next = NULL;
+        curr->next = NULL;
         delete curr;
-
     }
-
 }
 
-bool isCircularList(Node* head) {
-    //empty list
-    if(head == NULL) {
+bool isCircularList(Node *head)
+{
+    // empty list
+    if (head == NULL)
+    {
         return true;
     }
 
-    Node* temp = head -> next;
-    while(temp != NULL && temp != head ) {
-        temp = temp -> next;
+    Node *temp = head->next;
+    while (temp != NULL && temp != head)
+    {
+        temp = temp->next;
     }
 
-    if(temp == head ) {
+    if (temp == head)
+    {
         return true;
     }
 
     return false;
-
 }
 
-bool detectLoop(Node* head) {
+bool detectLoop(Node *head)
+{
 
-    if(head == NULL)
+    if (head == NULL)
         return false;
 
-    map<Node*, bool> visited;
+    map<Node *, bool> visited;
 
-    Node* temp = head;
+    Node *temp = head;
 
-    while(temp !=NULL) {
+    while (temp != NULL)
+    {
 
-        //cycle is present
-        if(visited[temp] == true) {
+        // cycle is present
+        if (visited[temp] == true)
+        {
             return true;
         }
 
         visited[temp] = true;
-        temp = temp -> next;
-
+        temp = temp->next;
     }
     return false;
-
 }
 
+int main()
+{
 
-int main() {
+    Node *tail = NULL;
 
-    Node* tail = NULL;
+    // insertNode(tail, 5, 3);
+    // print(tail);
 
-   // insertNode(tail, 5, 3);
-    //print(tail);
+    //  insertNode(tail, 3, 5);
+    // print(tail);
 
-  //  insertNode(tail, 3, 5);
-   // print(tail);
+    /*
+        insertNode(tail, 5, 7);
+        print(tail);
 
-/*
-    insertNode(tail, 5, 7);
-    print(tail);
+        insertNode(tail, 7, 9);
+        print(tail);
 
-    insertNode(tail, 7, 9);
-    print(tail);
+        insertNode(tail, 5, 6);
+        print(tail);
 
-    insertNode(tail, 5, 6);
-    print(tail);
-    
-    insertNode(tail, 9, 10);
-    print(tail);
+        insertNode(tail, 9, 10);
+        print(tail);
 
-    insertNode(tail, 3, 4);
-    print(tail);
-   
+        insertNode(tail, 3, 4);
+        print(tail);
 
-    deleteNode(tail, 5);
-    print(tail);
-     */
 
-    if(isCircularList(tail)) {
+        deleteNode(tail, 5);
+        print(tail);
+         */
+
+    if (isCircularList(tail))
+    {
         cout << " Linked List is Circular in nature" << endl;
     }
-    else{
+    else
+    {
         cout << "Linked List is not Circular " << endl;
     }
 
