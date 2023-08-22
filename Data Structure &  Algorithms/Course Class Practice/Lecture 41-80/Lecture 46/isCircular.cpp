14338dff30db28b744cf1de246b6b30aa794039f
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-22 19:55:04 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 41-80/Lecture 46/isCircular.cpp
@@ -0,0 +1,129 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    ~Node()
+    {
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+    }
+};
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+void insertAtPosition(Node *&head, Node *&tail, int position, int d)
+{
+    Node *temp = head;
+    int cnt = 1;
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void deleteNode(int position, Node *&head)
+{
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        Node *curr = head;
+        Node *prev = NULL;
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+bool isCircular(Node* head){
+    if(head==NULL){
+        return true;
+    }
+
+    Node*temp= head->next;
+    while(temp!=NULL && temp!=head){
+        temp=temp->next;
+    }
+    if(temp==head){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main()
+{
+    Node *node1 = new Node(4);
+    // cout << node1->data << endl;
+
+    Node *head = node1;
+    Node *tail = node1;
+    // print(head);
+    // insertAtHead(head, 12);
+    // print(head);
+    insertAtTail(tail, 20);
+    insertAtTail(tail, 25);
+    // print(head);
+    insertAtPosition(head, tail, 3, 22);
+    print(head);
+    deleteNode(3, head);
+    print(head);
+    print(tail);
+return 0;
+}
