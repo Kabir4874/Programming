2e4038634ed2eaf1a66c218f413e582f54e121fc
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-25 18:10:13 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 41-80/Lecture 55/reverseString.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    string str = "Kabir";
+    stack<char> s;
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+        s.push(ch);
+    }
+    string ans = "";
+    while (!s.empty())
+    {
+        char ch = s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+    cout << ans << endl;
+    return 0;
+}
