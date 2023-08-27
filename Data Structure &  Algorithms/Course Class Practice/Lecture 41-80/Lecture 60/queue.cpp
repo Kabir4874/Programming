c89c130e0abff2d4616947637647a04124206c89
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-27 12:34:45 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 41-80/Lecture 60/queue.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    queue<int> q;
+    q.push(11);
+    cout << "Front of queue is: " << q.front() << endl;
+    q.push(15);
+    q.push(13);
+    cout << "Size: " << q.size() << endl;
+    q.pop();
+    cout << "Size: " << q.size() << endl;
+    return 0;
+}
--- JAVA Learning/Final Exam/src/New/test.java
+++ JAVA Learning/Final Exam/src/New/test.java
@@ -7,7 +7,7 @@ public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
-        int k = n * 2 - 1;
+        int k = 1;
         for(int i=1; i<=n*2; i++)
         {
 
@@ -17,7 +17,7 @@ public class test {
             }
             if (i % 2 == 0)
             {
-                k -= 2;
+                k += 2;
             }
             System.out.println();
         }
