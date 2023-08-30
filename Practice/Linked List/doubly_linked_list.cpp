c28c6e0ab7a4ea237fa3d592d1f323284159da6a
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-08-31 01:06:29 +0600

    add

--- Practice/Linked List/doubly_linked_list.cpp
+++ Practice/Linked List/doubly_linked_list.cpp
@@ -13,6 +13,14 @@ public:
         this->prev = NULL;
         this->next = NULL;
     }
+    ~Node()
+    {
+        if (next != NULL)
+        {
+            delete next;
+            next = NULL;
+        }
+    }
 };
 void print(Node *head)
 {
@@ -24,10 +32,105 @@ void print(Node *head)
     }
     cout << endl;
 }
+int getLength(Node *head)
+{
+    Node *temp = head;
+    int cnt = 0;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+}
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+}
+void insertAtPosition(Node *&head, Node *&tail, int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+    Node *temp = head;
+    int cnt = 1;
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next->prev = nodeToInsert;
+    temp->next = nodeToInsert;
+    nodeToInsert->prev = temp;
+}
+void deleteNode(Node *&head, Node *&tail, int position)
+{
+    if (position == 1)
+    {
+        Node *temp = head;
+        temp->next->prev = NULL;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        Node *curr = head;
+        Node *prev = NULL;
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        curr->prev = NULL;
+        prev->next = curr->next;
+        curr->next = NULL;
+        if (prev->next == NULL)
+        {
+            tail = prev;
+            tail->next = NULL;
+        }
+
+        delete curr;
+    }
+}
 int main()
 {
     Node *node1 = new Node(10);
     Node *head = node1;
+    Node *tail = node1;
+    // print(head);
+    // int len = getLength(head);
+    // cout << len << endl;
+    insertAtHead(head, 20);
+    insertAtHead(head, 30);
+    // print(head);
+    insertAtTail(tail, 40);
+    print(head);
+    insertAtPosition(head, tail, 5, 50);
+    print(head);
+    deleteNode(head, tail, 5);
     print(head);
     return 0;
 }
