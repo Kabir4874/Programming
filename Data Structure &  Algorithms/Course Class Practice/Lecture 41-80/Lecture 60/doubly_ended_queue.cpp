1b119a6103fecb317fbce942d718cff22c073b69
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-09-02 21:09:39 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 41-80/Lecture 60/doubly_ended_queue.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    deque<int> d;
+    d.push_front(12);
+    d.push_back(14);
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+    return 0;
+}
