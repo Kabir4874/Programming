8b857a3625975813f37e0679af05ba890a107293
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-09-10 20:55:44 +0600

    add

---
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 81-120/Lecture 102/fibonacci_space_optimized.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    int n;
+    cin >> n;
+    int prev1 = 1;
+    int prev2 = 0;
+    if (n == 0)
+    {
+        cout << 0 << endl;
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    cout << prev1 << endl;
+    return 0;
+}
