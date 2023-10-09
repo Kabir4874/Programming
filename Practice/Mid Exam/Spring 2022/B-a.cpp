def4c3c5e7eb5d015a99c80198c032c09ea447bb
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-10-09 20:29:00 +0600

    add

--- Practice/Mid Exam/Spring 2022/B-a.cpp
+++ Practice/Mid Exam/Spring 2022/B-a.cpp
@@ -14,11 +14,30 @@ public:
         this->c = NULL;
     }
 };
-void insertAtLast(Node *head, float d)
+void insertAtLast(Node *&tail, float d)
 {
+    Node *temp = new Node(d);
+    tail->c = temp;
+    tail->b = temp->b;
+    tail = temp;
+}
+void display(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->k << " ";
+        temp = temp->c;
+    }
+    cout << endl;
 }
 int main()
 {
-
+    Node *n = new Node(14.5);
+    Node *head = n;
+    Node *tail = n;
+    insertAtLast(tail, 17.5);
+    insertAtLast(tail, 10.5);
+    display(head);
     return 0;
 }
