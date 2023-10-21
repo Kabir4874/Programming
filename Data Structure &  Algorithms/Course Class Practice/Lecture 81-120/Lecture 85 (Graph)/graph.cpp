292452b75c20ba50242066a0639181b1fb1af6bc
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2023-10-21 22:28:02 +0600

    add

--- Data Structure &  Algorithms/Course Class Practice/Lecture 81-120/Lecture 85 (Graph)/graph.cpp
+++ Data Structure &  Algorithms/Course Class Practice/Lecture 81-120/Lecture 85 (Graph)/graph.cpp
@@ -9,7 +9,7 @@ public:
     void addEdge(T u, T v, bool direction)
     {
         adj[u].push_back(v);
-        if (direction == 0)
+        if (direction == false)
         {
             adj[v].push_back(u);
         }
@@ -18,7 +18,7 @@ public:
     {
         for (auto i : adj)
         {
-            cout << i.first << "->";
+            cout << i.first << "-> ";
             for (auto j : i.second)
             {
                 cout << j << ", ";
@@ -30,19 +30,18 @@ public:
 int main()
 {
     int n;
-    cout << "Enter the number or Nodes: ";
+    cout << "Enter the number of Nodes: ";
     cin >> n;
     int m;
-    cout << "Enter the number of edges: ";
+    cout << "Enter the number of Edges: ";
     cin >> m;
     graph<int> g;
     for (int i = 0; i < m; i++)
     {
         int u, v;
         cin >> u >> v;
-        g.addEdge(u, v, 0);
+        g.addEdge(u, v, false);
     }
     g.printAdjList();
-
     return 0;
 }
