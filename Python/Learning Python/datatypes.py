359faa8c256eb8e3acb138a35704fd7121c2f0f1
Kabir4874 <kabir.cse.bd@gmail.com>
2024-01-22 22:10:59 +0600

    add

---
+++ Python/Learning Python/datatypes.py
@@ -0,0 +1,15 @@
+a=1
+print(a)
+print("Type of a is ",type(a))
+b=True
+print(b)
+c= "Kabir"
+print(c)
+d=None
+print(type(d))
+x=complex(8,2)
+print(x)
+list1=[8,2.3,[-4,5],["apple","banana"]]
+print(list1)
+dict1={"name":"kabir","age":22,"canVote":True}
+print(dict1)
