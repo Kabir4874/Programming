1e107bde12ee4bcc7ed86ea19be742e0b8e5f433
Kabir4874 <kabir.cse.bd@gmail.com>
2024-01-27 21:32:35 +0600

    add

---
+++ Python/Learning Python/f_strings.py
@@ -0,0 +1,9 @@
+letter= "Hey my name is {} and I am from {}"
+country= "Bangladesh"
+name="Kabir"
+print(letter.format(name,country))
+print(f"Hey my name is {name} and I am from {country}")
+
+price= 49.0999
+txt= f"For only {price:.2f} dollars!"
+print(txt)
